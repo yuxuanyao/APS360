fefe3fce449eea5da0af96527aa04faa32eb2a3c
Connie Yu <conniepiaopiao@gmail.com>
2020-11-09 12:42:37 -0500

    cleanup flask

--- app.py
+++ app.py
@@ -3,19 +3,19 @@ from music_recommender import MusicRecommender
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
 def index():
-    return render_template('index.html')
+    return render_template("index.html")
 
-@app.route("/", methods=['POST'])
+@app.route("/", methods=["POST"])
 def music_recommender():
     # run ml here:
-	# mood = model()
+    # # mood = model()
 
     recommender = MusicRecommender("username", "mood")
     playlist_id = recommender.get_playlist()
 
-    return render_template('playlist.html', playlist_id=playlist_id)
+    return render_template("playlist.html", playlist_id=playlist_id)
 
 if __name__ == "__main__":
 	app.run()
